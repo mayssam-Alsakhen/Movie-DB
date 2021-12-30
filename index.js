040b5a412ebfa53f4b43ae9db583a806259c833b
mayssam <mayssamalsakhen17@gmail.com>
2021-12-30 14:59:55 +0200

    step 5

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب', year: 1992, rating: 6.2 }
+]
 
 app.get(['/hello', '/hello/:id'] , (req, res) => {
     res.status(200).send({status:200, message:`Hello, ${req.params.id}`}) 
 })
+
+app.get(['/movie/create']), (req,res) =>{
+    res.send()
+}
+
+app.get(['/movies/read']), (req,res) =>{
+    res.send( {status:200, data: movies} )
+}
+
+app.get(['/movies/update']), (req,res) =>{
+    res.send()
+}
+
+app.get(['/movies/delete']), (req,res) =>{
+    res.send()
+}
+
+
 
 
 app.get('/search' , (req, res) => {

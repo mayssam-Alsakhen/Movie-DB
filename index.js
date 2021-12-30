3b3d8f8094602e421b78c09387dd5addaa6b19f5
mayssam <mayssamalsakhen17@gmail.com>
2021-12-30 12:11:07 +0200

    edit step 3

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/test', (req, res) =>  res.send('{status:200, message:"ok"}'))
+
+
+app.get('/test', (req, res) =>  res.send({status:200, message:"ok"}))
 
 app.get('/time', (req, res)  => { 
  var date = new Date();

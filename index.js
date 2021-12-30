6d4cb71d78e700db06ef15dc976dd2a97dbae771
mayssam <mayssamalsakhen17@gmail.com>
2021-12-30 14:46:41 +0200

    step 4

--- index.js
+++ index.js
@@ -2,16 +2,28 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.get(['/hello', '/hello/:id'] , (req, res) => {
+    res.status(200).send({status:200, message:`Hello, ${req.params.id}`}) 
+})
 
 
-app.get('/test', (req, res) =>  res.send({status:200, message:"ok"}))
+app.get('/search' , (req, res) => {
+    if(req.query.s){
+        res.status(200).send({status:200, message:"ok", data:`${req.query.s}`})
+    }
+    else{
+        res.status(500).send({status:500, error:true, message:"you have to provide a search"} )
+    }
+})
+
+app.get('/test', (req, res) =>  res.status(200).send({status:200, message:"ok"}))
 
 app.get('/time', (req, res)  => { 
  var date = new Date();
  var time = date.getHours() + ":" + date.getSeconds();
 
 
-    res.send({status:200, message: time })})
+    res.status(200).send({status:200, message: time })})
 
 
 app.get('/', (req, res) => res.send('ok'))

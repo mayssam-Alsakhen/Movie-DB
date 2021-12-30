7f29981daebfee19e2505bc34fd295d73be0c7bc
mayssam <mayssamalsakhen17@gmail.com>
2021-12-30 11:42:07 +0200

    step 3

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.get('/test', (req, res) =>  res.send('{status:200, message:"ok"}'))
+
+app.get('/time', (req, res)  => { 
+ var date = new Date();
+ var time = date.getHours() + ":" + date.getSeconds();
+
+
+    res.send({status:200, message: time })})
+
 
 app.get('/', (req, res) => res.send('ok'))
 

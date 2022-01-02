9efc3a6ef9f88eb4e54f92f63a8f8a6e8e8598d5
mayssam <mayssamalsakhen17@gmail.com>
2022-01-02 20:43:15 +0200

    step 8

--- index.js
+++ index.js
@@ -8,32 +8,32 @@ const movies = [
     { title: 'الإرهاب والكباب', year: 1992, rating: 6.2 }
 ]
 
-app.get(['/hello', '/hello/:id'] , (req, res) => {
+app.get('/hello', '/hello/:id', (req, res) => {
     res.status(200).send({status:200, message:`Hello, ${req.params.id}`}) 
 })
 
-app.get(['/movie/create'], (req,res) =>{
+app.get('/movie/create', (req,res) =>{
     res.send()
 })
 
-app.get(['/movies/read'], (req,res) =>{
-    res.send( {status:200, data: movies} )
+app.get('/movies/read', (req,res) =>{
+    res.status(200).send( {status:200, data: movies} )
 })
 
 
 app.get('/movies/read/by-date' , (req, res) => {
-    res.send({status:200, data: movies.sort((a,b) => b.year - a.year)})
+    res.status(200).send({status:200, data: movies.sort((a,b) => b.year - a.year)})
 })
 
-app.get(['/movies/read/by-rating'], (req, res) => {
-    res.send({status:200, data: movies.sort((a, b) => b.rating - a.rating)})
+app.get('/movies/read/by-rating', (req, res) => {
+    res.status(200).send({status:200, data: movies.sort((a, b) => b.rating - a.rating)})
 })
 
-app.get([' /movies/read/by-title'], (req,res) => {
+app.get(' /movies/read/by-title', (req,res) => {
     res.send({status:200, data: movies.sort((a,b) => a.title.localeCompare(b.title))})
 })
 
-app.get(['/movies/read/id/:id'], (req,res) => {
+app.get('/movies/read/id/:id', (req,res) => {
     if(req.params.id){
         if(Number(req.params.id) >=0 && (req.params.id) < movies.length){
             res.send({status:200, data:[res.params.id]})
@@ -45,12 +45,29 @@ app.get(['/movies/read/id/:id'], (req,res) => {
         res.status(500).send({status:500, error:true, message:"what is the id ...?"})
     }
 })
+
+app.get('movies/add', (req,res) => {
+
+    if(req.query.title && req.query.year && req.query.year.length == 4 && !isNaN(req.query.year)){
+     let newMovie={
+         title: req.query.title, 
+         year: req.query.year,
+         rating : req.query.rating ? req.query.rating : 4}
+    
+         movies.push(newMovie)
+         res.status(200).send({status: 200, data: movies})
+         
+    }else{
+    
+     res.status(403).send({status:403, error:true, message:'you cannot create a movie without providing a title and a year'})
+    }
+    })
  
-app.get(['/movies/update'], (req,res) =>{
+app.get('/movies/update', (req,res) =>{
     res.send()
 })
 
-app.get(['/movies/delete'], (req,res) =>{
+app.get('/movies/delete', (req,res) =>{
     res.send()
 })
 
@@ -73,7 +90,11 @@ app.get('/time', (req, res)  => {
  var time = date.getHours() + ":" + date.getSeconds();
 
 
-    res.status(200).send({status:200, message: time })})
+    res.status(200).send({status:200, message: time })
+})
+
+
+
 
 
 app.get('/', (req, res) => res.send('ok'))
